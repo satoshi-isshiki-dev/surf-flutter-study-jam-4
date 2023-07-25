71e8e70c409a71318c957939b7dd447841007e33
Satoshi Isshiki (一色 慧) <satoshi.dev@yandex.ru>
2023-07-25 21:52:58 +0700

    Added CORS options & ShakeDetector

--- lib/screen/magic_ball_screen/magic_ball_screen.dart
+++ lib/screen/magic_ball_screen/magic_ball_screen.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shake/shake.dart';
 import 'package:surf_practice_magic_ball/screen/magic_ball_screen/bloc/magic_ball_bloc.dart';
 import 'package:surf_practice_magic_ball/screen/magic_ball_screen/widgets/magic_ball_widget.dart';
 
-class MagicBallScreen extends StatelessWidget {
+class MagicBallScreen extends StatefulWidget {
   const MagicBallScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final _magicBallBloc = MagicBallBloc();
+  State<MagicBallScreen> createState() => _MagicBallScreenState();
+}
 
+class _MagicBallScreenState extends State<MagicBallScreen> {
+  final magicBallBloc = MagicBallBloc();
+  late ShakeDetector detector;
+
+  @override
+  void initState() {
+    detector = ShakeDetector.autoStart(
+      onPhoneShake: () {
+        magicBallBloc.add(LoadMagicBallMessage());
+      },
+      minimumShakeCount: 1,
+      shakeSlopTimeMS: 500,
+      shakeCountResetTime: 3000,
+      shakeThresholdGravity: 2.7,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -24,51 +45,56 @@ class MagicBallScreen extends StatelessWidget {
           ),
         ),
         child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: BlocBuilder(
-                bloc: _magicBallBloc,
-                builder: (context, state) {
-                  if (state is MagicBallMessageLoaded) {
-                    return MagicBallWidget(
-                        magicBallBloc: _magicBallBloc,
-                        magicBallItems: [
-                          Image.asset('assets/images/ball.png',
-                              fit: BoxFit.contain),
-                          Text(
-                            state.reading,
-                            style: const TextStyle(
-                              fontSize: 32.0,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                        error: false);
-                  }
-                  if (state is MagicBallMessageLoadingFailure) {
-                    return MagicBallWidget(
-                        magicBallBloc: _magicBallBloc,
-                        magicBallItems: [
-                          Image.asset('assets/images/error/ball.png',
-                              fit: BoxFit.contain),
-                          Image.asset('assets/images/star.png',
-                              fit: BoxFit.contain),
-                          Image.asset('assets/images/small star.png',
-                              fit: BoxFit.contain),
-                        ],
-                        error: true);
-                  }
-                  return MagicBallWidget(
-                      magicBallBloc: _magicBallBloc,
-                      magicBallItems: [
-                        Image.asset('assets/images/ball.png',
-                            fit: BoxFit.contain),
-                        Image.asset('assets/images/star.png',
-                            fit: BoxFit.contain),
-                        Image.asset('assets/images/small star.png',
-                            fit: BoxFit.contain),
-                      ],
-                      error: false);
-                })),
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: BlocBuilder(
+            bloc: magicBallBloc,
+            builder: (context, state) {
+              if (state is MagicBallMessageLoaded) {
+                return MagicBallWidget(
+                  magicBallBloc: magicBallBloc,
+                  magicBallItems: [
+                    Image.asset('assets/images/ball.png', fit: BoxFit.contain),
+                    Text(
+                      state.reading,
+                      style: const TextStyle(
+                        fontSize: 32.0,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                  error: false,
+                );
+              }
+              if (state is MagicBallMessageLoadingFailure) {
+                return MagicBallWidget(
+                  magicBallBloc: magicBallBloc,
+                  magicBallItems: [
+                    Image.asset('assets/images/error/ball.png',
+                        fit: BoxFit.contain),
+                    Image.asset('assets/images/star.png', fit: BoxFit.contain),
+                    Image.asset('assets/images/small star.png',
+                        fit: BoxFit.contain),
+                    Text(
+                      state.exception.toString(),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                  error: true,
+                );
+              }
+              return MagicBallWidget(
+                magicBallBloc: magicBallBloc,
+                magicBallItems: [
+                  Image.asset('assets/images/ball.png', fit: BoxFit.contain),
+                  Image.asset('assets/images/star.png', fit: BoxFit.contain),
+                  Image.asset('assets/images/small star.png',
+                      fit: BoxFit.contain),
+                ],
+                error: false,
+              );
+            },
+          ),
+        ),
       ),
     );
   }
